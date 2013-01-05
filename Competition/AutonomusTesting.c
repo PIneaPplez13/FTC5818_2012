ff6b8b6c9118e7e491bcd9f75f3542b0f77bac4e
Joe Quigley <joecool.17.3@gmail.com>
2013-01-05 13:28:50 -0500

    fixing it

--- Competition/AutonomusTesting.c
+++ Competition/AutonomusTesting.c
@@ -23,8 +23,12 @@ typedef struct {
 } tStepArray;
 
 tStepArray stp;
-bool cont = false;
+bool cont = true;
 int _nSteps;
+
+int calcMotorDrive(int joy, int adj)	{
+	return((int)((joy/127.0)*adj));
+}
 
 void init()	{
 	nxtDisplayCenteredTextLine(0, "STP FINDER");
@@ -32,8 +36,6 @@ void init()	{
 }
 
 void update()	{
-	getJoystickSettings(joystick);
-
 	if(joy1Btn(1))	{
 		nMotorEncoder[leftFront] = 0;
 		nMotorEncoder[leftRear] = 0;
@@ -43,10 +45,10 @@ void update()	{
 		nxtDisplayCenteredTextLine(7, "ENC reset.");
 	}
 	else if(joy1Btn(2))	{
-		stp.step[stp.nSteps][0] = nMotorEncoder[leftFront] = 0;
-		stp.step[stp.nSteps][1] = nMotorEncoder[leftRear] = 0;
-		stp.step[stp.nSteps][2] = nMotorEncoder[rightFront] = 0;
-		stp.step[stp.nSteps][3] = nMotorEncoder[rightRear] = 0;
+		stp.step[stp.nSteps][0] = nMotorEncoder[leftFront];// = 0;
+		stp.step[stp.nSteps][1] = nMotorEncoder[leftRear];// = 0;
+		stp.step[stp.nSteps][2] = nMotorEncoder[rightFront];// = 0;
+		stp.step[stp.nSteps][3] = nMotorEncoder[rightRear];// = 0;
 		nxtDisplayCenteredTextLine(7, "STP %i/64", stp.nSteps);
 		writeDebugStream("New step registered: %i/64", stp.nSteps);
 		writeDebugStream("\tLeft Front:\t%i\n\tLeft Rear:\t%i", stp.step[stp.nSteps][0], stp.step[stp.nSteps][1]);
@@ -63,12 +65,12 @@ void update()	{
 		writeDebugStream("Scissor Motors:\n\tScissorLeft:\t%i\n\tScissorRight:\t%i", nMotorEncoder[scissorLeft], nMotorEncoder[scissorRight]);
 	}
 
-	motor[leftFront] = joy1_y1;
-	motor[leftRear] = joy1_y1;
-	motor[rightFront] = joy1_y2;
-	motor[rightRear] = joy1_y2;
+	motor[leftRear] = calcMotorDrive(joystick.joy1_y1, 100);
+	motor[leftFront] = calcMotorDrive(joystick.joy1_y1, 100);
+	motor[rightRear] = calcMotorDrive(joystick.joy1_y2, 100);
+	motor[rightFront] = calcMotorDrive(joystick.joy1_y2, 100);
 
-	if(joy1Btn(1))	{
+	if(joy1Btn(10))	{
 		cont = false;
 	}
 }
@@ -81,6 +83,7 @@ task main()
 
 	nNxtExitClicks = 2;
 	while(cont)	{
+		getJoystickSettings(joystick);
 		update();
 	}
 
@@ -92,9 +95,9 @@ task main()
 		if(nNxtButtonPressed == 3)	{
 			nxtDisplayString(0, "STEP %i/%i", _nSteps, stp.nSteps);
 			nxtDisplayString(1, "ENC1: %i", stp.step[_nSteps][0]);
-			nxtDisplayString(1, "ENC2: %i", stp.step[_nSteps][1]);
-			nxtDisplayString(1, "ENC3: %i", stp.step[_nSteps][2]);
-			nxtDisplayString(1, "ENC4: %i", stp.step[_nSteps][3]);
+			nxtDisplayString(2, "ENC2: %i", stp.step[_nSteps][1]);
+			nxtDisplayString(3, "ENC3: %i", stp.step[_nSteps][2]);
+			nxtDisplayString(4, "ENC4: %i", stp.step[_nSteps][3]);
 		}
 		else if(nNxtButtonPressed == 1)	{
 			_nSteps++;
